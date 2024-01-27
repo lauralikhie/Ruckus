b8184aaf5b98b291e39a3e769d0690d2dc35b0cb
lauralikhie <40911646+lauralikhie@users.noreply.github.com>
2024-01-27 10:12:42 +0530

    Add files via upload

---
+++ aivenprj/src/main/java/com/aiven/lance/prj/aivenprj/AivenprjApplication.java
@@ -0,0 +1,13 @@
+package com.aiven.lance.prj.aivenprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AivenprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AivenprjApplication.class, args);
+	}
+ //Initial commit
+}
---
+++ aivenprj/src/test/java/com/aiven/lance/prj/aivenprj/AivenprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiven.lance.prj.aivenprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AivenprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
